dbbdfc6f8b11d4841cda2629a6f5e46ac66cc84a
Sebastian Obando <sebastiano-10@hotmail.com>
2020-03-16 15:07:28 -0500

    Finished Project

--- src/database.js
+++ src/database.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
 const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env;
-//const MONGODB_URI=`mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+const MONGODB_URI=`mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
-const MONGODB_URI='mongodb+srv://seobando:13104043@cluster0-ftrso.mongodb.net/test?retryWrites=true&w=majority'
 
 mongoose.connect(MONGODB_URI,{
     useNewUrlParser:true,
